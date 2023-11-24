8460ec552773979509d429039b116257dfe14491
Yuri Salgado Farias Torres <yuri.salgado.ft@gmail.com>
2023-11-24 18:18:30 -0300

    Ajeitei o meu controller

--- backend/src/controllers/sales.controller.js
+++ backend/src/controllers/sales.controller.js
@@ -2,7 +2,7 @@ const { salesService } = require('../services');
 const mapStatusHTTP = require('../utils/mapStatusHTTP');
 
 const createSalesProduct = async (req, res) => {
-  const salesArray = { ...req.body };
+  const salesArray = req.body;
   const { status, data } = await salesService.createSales(salesArray);
   return res.status(mapStatusHTTP(status)).json(data);
 };
